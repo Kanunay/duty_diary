24b642e53b692f9c8e818f3faff5811db713e3a7
philip <philipelizaga72@gmail.com>
2023-08-08 12:16:49 +0800

    -m

--- resources/views/admin/diaries/index.blade.php
+++ resources/views/admin/diaries/index.blade.php
@@ -1,7 +1,42 @@
 @extends('layouts.admin')
 
-
 @section("content")
-<h1>Diaries</h1>
-@endsection
 
+<section class="vh" style="background-color: #eee;">
+  <div class="row d-flex justify-content-center align-items-center h-200">
+      <div class="col col-lg-11 col-x2-7">
+          <div class="card rounded-3">
+              <div class="card-body p-4">
+                  <div class="card-header">
+                      <div class="row">
+                          <div class="col-md-6 col-12">
+                              <i class="fas fa-solid fa-users"></i>
+                              Diaries
+                          </div>
+                          <div class="col-md-6 col-12 text-right">
+                              <a href="" class="btn btn-sm btn-primary ">Add User</a>
+                          </div>
+                      </div>
+                  </div>
+
+                  <table class="table mb-4">
+                      <thead>
+                          <tr>
+                              <th scope="col">#</th>
+                              <th scope="col">Actions</th>
+                              <th scope="col">Title</th>
+                              <th scope="col">Status</th>   
+                          </tr>
+                      </thead>
+                      <tbody>
+                          
+                      </tbody>
+                  </table>
+
+              </div>
+          </div>
+      </div>
+  </div>
+  
+</section>
+@endsection
